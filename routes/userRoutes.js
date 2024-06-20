13b051aaa45ee18752793ecccc53def9da834140
Sidhu_roy1371 <bhaskarbunny1371@gmail.com>
2024-06-20 16:47:30 +0530

    error handling

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -55,7 +55,7 @@ userRouter.post("/checkout-session", async (req, res) => {
     res.json({ id: session.id });
   } catch (error) {
     console.error(error);
-    res.status(500).send("Internal Server Error");
+    res.status(500).send(error);
   }
 });
 
